9e9d8e526120e1ed701974471fb8dc30340dc1cd
s24026 <s24026@std.it-college.ac.jp>
2024-07-09 06:35:32 +0000

    2024.7.9

---
+++ CH2-12.py
@@ -0,0 +1,14 @@
+import random
+print(random.randint(0,10))
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','s','y','z']
+print(random.choice(alphabet))
+
+print(random.sample(alphabet,5))
+
+from random import randint, choice
+print(randint(0,10))
+print(choice(['a','b','c']))
+
+print(random.randint(0,10))
+print(randint(0,10))
