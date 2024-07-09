2d2b895452d08c08c68eb3bd426416bd04902c0d
s24026 <s24026@std.it-college.ac.jp>
2024-07-09 08:01:44 +0000

    2024.7.9

---
+++ CH3/CH3-2.py
@@ -0,0 +1,17 @@
+a = 30 
+if a % 15 == 0:
+    print('fifteen')
+elif a % 3 == 0:
+    print('three')
+elif a % 5 == 0:
+    print('five')
+else:
+    pass
+
+a,b = 10,20
+if a > 10 or  b < 10:
+    print('ok')
+else:
+    print('ng')
+
+
